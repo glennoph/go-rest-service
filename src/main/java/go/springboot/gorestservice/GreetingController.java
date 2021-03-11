5c4cf360c46d46ef40c20d17a34327076ece2d03
glenn opdycke-hansen <glennoph@gmail.com>
2021-03-11 16:22:23 -0600

    add slf4j logger

--- src/main/java/go/springboot/gorestservice/GreetingController.java
+++ src/main/java/go/springboot/gorestservice/GreetingController.java
@@ -1,5 +1,7 @@
 package go.springboot.gorestservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +25,12 @@ public class GreetingController {
     @Value("${data.id:defaultid}")
     private  String dataId ;
 
+    private final static Logger log = LoggerFactory.getLogger(GreetingController.class);
+
     @GetMapping("/greeting")
     public Greeting greeting(
             @RequestParam(value = "name", defaultValue = "World") String name) {
+        log.info("/greeting name:"+name);
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name ));
     }
@@ -34,6 +39,7 @@ public class GreetingController {
     public Greeting greetingId(
             @RequestParam(value = "name", defaultValue = "World") String name) {
         // return greeting with name, dataName, and dataId
+        log.info("/greetingid dataname:"+dataName+" id:"+dataId);
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name + " dataName: " + dataName + " dataId: " + dataId));
     }
